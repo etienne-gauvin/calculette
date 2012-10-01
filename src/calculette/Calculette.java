e6516507295297883aac233e653cd892e50ba964
Etienne Gauvin <etiennegauvin41@gmail.com>
2012-10-02 00:28:07 +0200

    First commit

---
+++ src/calculette/Calculette.java
@@ -0,0 +1,49 @@
+package calculette;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+/**
+ *
+ * @author etienne
+ */
+
+public class Calculette
+{
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args)
+    {
+        try
+        {
+            // Chargement du L&F Nimbus
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            
+            // Créer la fenêtre
+            CalculetteFrame calculetteFrame = new CalculetteFrame();
+            
+            // Chargement de l'icône
+            Image icon = Toolkit.getDefaultToolkit().getImage("icon.png");
+            calculetteFrame.setIconImage(icon);
+            
+            // Afficher la fenêtre
+            calculetteFrame.setLocationRelativeTo(null);
+            calculetteFrame.setVisible(true);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
